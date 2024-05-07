7b675a52241d8fcba91fbe10d07d6595ee00dc30
Khalil Hersi <khalilhersi2000@gmail.com>
2024-05-07 16:38:34 +0100

    CC

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Course.destroy_all
+User.destroy_all
 
 user1 = User.create!(email: 'test@email.com', password: '123456')
+user2 = User.create!(email: 'another_test@anotheremail.com', password: '123456')
+
+course1 = Course.create!(subject: 'Maths', user_id: user1.id)
+course2 = Course.create!(subject: 'Chemistry', user_id: user1.id)
+course3 = Course.create!(subject: 'English', user_id: user1.id)
+course4 = Course.create!(subject: 'Physics', user_id: user1.id)
+course5 = Course.create!(subject: 'Arabic', user_id: user1.id)
