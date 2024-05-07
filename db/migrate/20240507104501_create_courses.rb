d7fdbd8699a0b0d553bf294148f7e068819389ba
Gianna Sharni <GiannaSharni@gmail.com>
2024-05-07 11:59:35 +0100

    user fixed

--- db/migrate/20240507104501_create_courses.rb
+++ db/migrate/20240507104501_create_courses.rb
@@ -2,7 +2,7 @@ class CreateCourses < ActiveRecord::Migration[7.1]
   def change
     create_table :courses do |t|
       t.string :subject
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
