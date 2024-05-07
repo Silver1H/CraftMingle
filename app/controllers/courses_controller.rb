04f7869448411984be4efdc8c2e74a9b252b8b67
Khalil Hersi <162932034+Silver1H@users.noreply.github.com>
2024-05-07 16:39:16 +0100

    Revert "courses and users"

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,5 @@
 class CoursesController < ApplicationController
-  def new
-    @course = Course.new
-  end
-
-  def create
-    current_user.courses.create!(subject: params['course']['subject'])
-
-    redirect_to root_url
+  def index
+    
   end
 end
