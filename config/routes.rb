4601c2a8e6d1da505a86de4b134fa0bcf4623439
Gianna Sharni <GiannaSharni@gmail.com>
2024-05-07 12:23:41 +0100

    route started for courses index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "courses#index"
+  resources :courses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,3 +11,21 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+
+# /classes	GET	classes	index
+# /classes	GET	classes	index
+
+
+# /classes/:id	GET	classes	show
+# /classes/:id/bookings/new	GET	bookings	new
+# /classes/:id/bookings	POST	bookings	create
+
+
+# /bookings	GET	bookings	index
+
+# /classes/new	GET	classes	new
+# /classes	POST	classes	create
+
+
+# /dashboards	GET	dashboards	index
