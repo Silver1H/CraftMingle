6ed53ab6867869d493a91ea804cc953b5a213dab
Rumaysaa Mohamed <rumaysaa15@gmail.com>
2024-05-10 13:04:16 +0100

    search bar done

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,11 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  include PgSearch::Model
+  pg_search_scope :search_by_subject,
+    against: [ :subject ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
